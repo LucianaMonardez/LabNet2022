1d14df33757182ad2d90b44674189f6af6e968bf
LucianaMonardez <lucianapiomonardez@gmail.com>
2022-05-23 22:46:42 -0300

    Commit hello world

---
+++ Lab2022.HelloWorld/Lab2022.HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2022.HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Presiona cualquier tecla para cerrar la ventana...");
+            Console.ReadKey();
+        }
+    }
+}
