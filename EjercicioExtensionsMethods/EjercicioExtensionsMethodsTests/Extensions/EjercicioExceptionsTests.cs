4cd943bcbbc16d73c5142aacb8d811009583ffe8
LucianaMonardez <lucianapiomonardez@gmail.com>
2022-06-01 11:42:33 -0300

    Agrego tests

---
+++ EjercicioExtensionsMethods/EjercicioExtensionsMethodsTests/Extensions/EjercicioExceptionsTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EjercicioExtensionsMethods.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExtensionsMethods.Extensions.Tests
+{
+    [TestClass()]
+    public class EjercicioExceptionsTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException), "Attempted to divide by zero.")]
+        public void DividirPorCeroTestExepcion()
+        {
+            //arrange
+            int numero1 = 3;
+            int numero2 = 0;
+            //act
+            var resultado = numero1 / numero2;
+
+        }
+
+        [TestMethod()]
+        public void DivisionConDosParametrosSuccess() 
+        {
+            //arrange
+            int numero1 = 10;
+            int numero2 = 2;
+            int esperado = 5;
+
+            //act
+            var resultado = numero1 / numero2;
+
+            //assert
+            Assert.AreEqual(resultado, esperado);
+        }
+        [TestMethod()]
+        public void DivisionConDosParametrosFail()
+        {
+            //arrange
+            int numero1 = 10;
+            int numero2 = 3;
+            int esperado = 5;
+
+            //act
+            var resultado = numero1 / numero2;
+
+            //assert
+            Assert.AreNotEqual(resultado, esperado);
+        }
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException), "Attempted to divide by zero.")]
+        public void DivisionConDosParametrosExepcionDivideByZeroException()
+        {
+            //arrange
+            int numero1 = 3;
+            int numero2 = 0;
+            //act
+            var resultado = numero1 / numero2;
+
+        }
+    }
+}
---
+++ EjercicioExtensionsMethods/EjercicioExtensionsMethodsTests/Logic/ClaseLogicTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EjercicioExtensionsMethods.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioExtensionsMethods.Logic.Tests
+{
+    [TestClass()]
+    public class ClaseLogicTests
+    {
+        [TestMethod()]
+        public void ValidarByteTestFail()
+        {
+            var numero = 300;
+            var esperado = 30;
+
+            Assert.AreNotEqual(numero, esperado);
+        }
+        [TestMethod()]
+        public void ValidarByteTestSuccess()
+        {
+            var numero = 30;
+            var esperado = 30;
+
+            Assert.AreEqual(numero, esperado);
+        }
+
+        [TestMethod()]
+        public void ValidarNumeroParTestSuccess()
+        {
+            var numero = 2;
+            var esperado = 2;
+
+            Assert.AreEqual(numero, esperado);
+        }
+        [TestMethod()]
+        public void ValidarNumeroParTestFail()
+        {
+            var numero = 2;
+            var esperado = 3;
+
+            Assert.AreNotEqual(numero, esperado);
+        }
+
+    }
+}
