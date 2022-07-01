bbb3b2e0e1124f52046b549ee3d339df74674ce4
LucianaMonardez <lucianapiomonardez@gmail.com>
2022-06-30 23:55:08 -0300

    Se activan los cors

--- PracticaEF/Northwind.EF.UI/Lab.Northwind.WebApi/App_Start/WebApiConfig.cs
+++ PracticaEF/Northwind.EF.UI/Lab.Northwind.WebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Lab.Northwind.WebApi
 {
@@ -9,8 +10,11 @@ namespace Lab.Northwind.WebApi
     {
         public static void Register(HttpConfiguration config)
         {
+            
             // Web API configuration and services
 
+            var cors = new EnableCorsAttribute("http://localhost:4200/ , *", headers: "*", methods: "*");
+            config.EnableCors(cors);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
